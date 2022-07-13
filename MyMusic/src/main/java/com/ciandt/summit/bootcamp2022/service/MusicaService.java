dda3d41aea3abe20a497d630c1fd7723e2709511
Guilherme.Castilho <gcastilho@ciandt.com>
2022-07-13 14:10:57 -0300

    Criação do serviço para buscar músicas ao receber um filtro

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicaService.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicaService.java
@@ -1,9 +1,11 @@
 package com.ciandt.summit.bootcamp2022.service;
 
 import com.ciandt.summit.bootcamp2022.entity.Musica;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface MusicaService {
 
     List<Musica> buscarMusicas(String filtro);
