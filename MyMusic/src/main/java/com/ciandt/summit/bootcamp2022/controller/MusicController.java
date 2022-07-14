9dba9dbe894fd15880a0073048763d4c41a5f93a
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-07-14 09:50:31 -0300

    Sprint1 - Criação do Controller.

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -1,16 +1,29 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
+import com.ciandt.summit.bootcamp2022.entity.Musica;
+import com.ciandt.summit.bootcamp2022.exception.MusicaNaoEncontradaException;
+import com.ciandt.summit.bootcamp2022.service.MusicaServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/music")
+@RequestMapping("/api")
 public class MusicController {
 
-    @GetMapping
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("67f5976c-eb1e-404e-8220-2c2a8a23be47");
+    @Autowired   private MusicaServiceImp musicaServiceImp;
+
+    @GetMapping("/musicas")
+    public ResponseEntity<List<Musica>> buscarMusica(@RequestParam String filtro){
+        List<Musica> musicas =  musicaServiceImp.buscarMusicas(filtro);
+        if(musicas.isEmpty()){
+            throw new MusicaNaoEncontradaException("Música não encontrada!");
+        }
+        return new ResponseEntity<List<Musica>>(musicas, HttpStatus.OK);
     }
 }
+
