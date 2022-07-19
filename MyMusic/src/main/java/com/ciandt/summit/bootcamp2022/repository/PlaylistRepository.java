2ec971bd43fe3d7ae9a99c4dc4ae4aa44f50e7b4
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-07-19 11:22:52 -0300

    repository

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/PlaylistRepository.java
@@ -0,0 +1,7 @@
+package com.ciandt.summit.bootcamp2022.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, String>{
+
+}
