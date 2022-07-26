79ff5e0badd5b9c6ec5b85c06395f0588699839d
julianacit <julianamt@ciandt.com>
2022-07-26 14:21:13 -0300

    Mapeamento controller e configuração swagger

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.ciandt.summit.bootcamp2022.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("API MyMusic").description(
+                        "Projeto final Bootcamp - grupo Sem Exceção - Turma Maio."));
+    }
+}
