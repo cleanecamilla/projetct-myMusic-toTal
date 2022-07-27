35c09de6a3a6b71aa2e3ef0205c50f9f2af53cfc
julianacit <julianamt@ciandt.com>
2022-07-27 10:20:19 -0300

    Problem with controller validation

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/DTO/MusicaDTO.java
@@ -0,0 +1,4 @@
+package com.ciandt.summit.bootcamp2022.DTO;
+
+public class MusicaDTO {
+}
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicaController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicaController.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/musicas")
@@ -17,16 +21,14 @@ public class MusicaController {
     //instância de service
 
     @GetMapping
+    @Validated
     @Operation(summary = "Buscar músicas/artistas",
             description = "Buscar músicas/artistas, não case sensitive e necessário 2 caracteres mínimo")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Operação bem sucedida"),
             @ApiResponse(responseCode = "204", description = "Sem resultados"),
             @ApiResponse(responseCode = "400", description = "Caracteres insuficientes")})
-    public ResponseEntity<?> buscarMusicas(@PathVariable(value = "filtro", required = false) String filtro) {
-        if (filtro != null && !filtro.isEmpty() && filtro.length() < 2) {
-            return ResponseEntity.badRequest().build();
-        }
+    public ResponseEntity<?> buscarMusicas(@Valid @RequestParam(name = "filtro", required = false) @Size(min = 2) String filtro) {
 
         //chamar service
 
