88d308f5a6ea68d89eaa7ecd4de1d1211e903eb1
Luis <luis_8.51@hotmail.com>
2022-07-29 10:04:33 -0300

    Adição Log

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/common/exception/service/InvalidParameterException.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/common/exception/service/InvalidParameterException.java
@@ -1,13 +1,15 @@
 package com.ciandt.summit.bootcamp2022.common.exception.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+@Log4j2
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidParameterException extends RuntimeException {
 
     public InvalidParameterException(String message) {
         super(message);
+        log.warn("Invalid Parameter {}", message);
     }
 
 }
