12983aee569941c81fd8bbca6c083aec28f1498d
gabivenerando <gabrielliv@ciandt.com>
2022-08-19 09:45:27 -0300

    added route

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/MusicController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/music")
+@RequestMapping("/api/musicas")
 public class MusicController {
 
     @GetMapping
-    public ResponseEntity<String> get() {
+    public ResponseEntity<String> getMusic() {
         return ResponseEntity.ok("67f5976c-eb1e-404e-8220-2c2a8a23be47");
     }
 }
