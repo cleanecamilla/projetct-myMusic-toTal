138c2ba9b6c166cfaf461dfb1f75cf9601689cf5
rsousa <rsousa@ciandt.com>
2022-08-22 15:31:36 -0300

    feat: SongRepository implemented

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/SongRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/SongRepository.java
@@ -2,10 +2,13 @@ package com.ciandt.summit.bootcamp2022.infra.adapters.repositories;
 
 import com.ciandt.summit.bootcamp2022.domains.songs.Song;
 import com.ciandt.summit.bootcamp2022.domains.songs.ports.repositories.SongRepositoryPort;
+import com.ciandt.summit.bootcamp2022.infra.adapters.entities.SongEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class SongRepository implements SongRepositoryPort {
@@ -14,8 +17,11 @@ public class SongRepository implements SongRepositoryPort {
 
     @Override
     public List<Song> findByNameOrArtistName(String name, int pageNumber) {
-        return null;
+        Pageable pageable = Pageable.ofSize(10).withPage(pageNumber);
+
+        return songRepository.findByNameOrArtistName(name, pageable)
+                .stream()
+                .map(SongEntity::toSong)
+                .collect(Collectors.toList());
     }
-
-
 }
