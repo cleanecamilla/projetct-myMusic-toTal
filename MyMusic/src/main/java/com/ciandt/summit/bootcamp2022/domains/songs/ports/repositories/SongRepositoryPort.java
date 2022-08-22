13790e094f0cf3c12410a459b8c1602604c44969
rsousa <rsousa@ciandt.com>
2022-08-22 09:53:23 -0300

    feat: methods of SongRepositoryPort and its implementation defined

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domains/songs/ports/repositories/SongRepositoryPort.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domains/songs/ports/repositories/SongRepositoryPort.java
@@ -1,4 +1,9 @@
 package com.ciandt.summit.bootcamp2022.domains.songs.ports.repositories;
 
+import com.ciandt.summit.bootcamp2022.domains.songs.Song;
+
+import java.util.List;
+
 public interface SongRepositoryPort {
+    List<Song> findByNameOrArtistName(String name, int pageNumber);
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/SongRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/SongRepository.java
@@ -1,11 +1,21 @@
 package com.ciandt.summit.bootcamp2022.infra.adapters.repositories;
 
+import com.ciandt.summit.bootcamp2022.domains.songs.Song;
 import com.ciandt.summit.bootcamp2022.domains.songs.ports.repositories.SongRepositoryPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SongRepository implements SongRepositoryPort {
     @Autowired
     private SpringSongRepository songRepository;
+
+    @Override
+    public List<Song> findByNameOrArtistName(String name, int pageNumber) {
+        return null;
+    }
+
+
 }
