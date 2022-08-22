179a6c632ac1bbb0aec38c04f3ec04a7548c90aa
Jenifer <jnakabar@ciandt.com>
2022-08-22 17:14:57 -0300

    refect:domain adjustment

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/entities/ArtistEntity.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/entities/ArtistEntity.java
@@ -1,14 +1,10 @@
 package com.ciandt.summit.bootcamp2022.infra.adapters.entities;
-
 import com.ciandt.summit.bootcamp2022.domains.artists.Artist;
-import com.ciandt.summit.bootcamp2022.domains.songs.Song;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -20,12 +16,14 @@ public class ArtistEntity {
     @Id
     @Column(name = "Id")
     private String id = UUID.randomUUID().toString();
+
     @Column(name = "Nome")
     private String name;
-     public ArtistEntity(Artist artist) {
+
+    public ArtistEntity(Artist artist) {
         this.id = artist.getId();
         this.name = artist.getName();
-        }
+    }
 
     public Artist toArtist() {
         return new Artist(this.id, this.name, new ArrayList<>());
