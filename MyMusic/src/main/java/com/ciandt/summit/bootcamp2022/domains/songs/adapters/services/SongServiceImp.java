26907a2252b8520cf479c1cfc74d88cbc76ca4e5
Joao Bohana <jbohana@ciandt.com>
2022-08-23 15:43:08 -0300

    feat: implemented method findByNameOrArtistName in the service layer

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domains/songs/adapters/services/SongServiceImp.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domains/songs/adapters/services/SongServiceImp.java
@@ -1,6 +1,45 @@
 package com.ciandt.summit.bootcamp2022.domains.songs.adapters.services;
 
+import com.ciandt.summit.bootcamp2022.domains.exceptions.songs.InvalidSongNameOrArtistNameException;
+import com.ciandt.summit.bootcamp2022.domains.exceptions.songs.SongsNotFoundException;
+import com.ciandt.summit.bootcamp2022.domains.songs.Song;
+import com.ciandt.summit.bootcamp2022.domains.songs.dtos.SongDTO;
 import com.ciandt.summit.bootcamp2022.domains.songs.ports.interfaces.SongServicePort;
+import com.ciandt.summit.bootcamp2022.domains.songs.ports.repositories.SongRepositoryPort;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SongServiceImp implements SongServicePort {
+    private final SongRepositoryPort songRepositoryPort;
+
+    public SongServiceImp(SongRepositoryPort songRepositoryPort) {
+        this.songRepositoryPort = songRepositoryPort;
+    }
+
+    @Override
+    public List<SongDTO> findByNameOrArtistName(String name, int pageNumber)
+            throws InvalidSongNameOrArtistNameException, SongsNotFoundException {
+
+        if(name == null || name.isBlank() || name.length() < 2){
+            throw new InvalidSongNameOrArtistNameException("Parameter must be at least 2 characters long.");
+        }
+
+        List<Song> songs = songRepositoryPort.findByNameOrArtistName(name, pageNumber);
+        return convertSongListToDTOList(songs);
+    }
+
+    private List<SongDTO> convertSongListToDTOList(List<Song> songs) throws SongsNotFoundException {
+        List<SongDTO> songsDTO = new ArrayList<>();
+
+        if(songs.isEmpty()){
+            throw new SongsNotFoundException("No songs were found.");
+        } else {
+            songs.forEach(song -> {
+                songsDTO.add(song.toDTO());
+            });
+        }
+
+        return songsDTO;
+    }
 }
