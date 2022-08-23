23ca89c8b66fe39ebd716273aa43b7f9c7c0aff7
rsousa <rsousa@ciandt.com>
2022-08-23 09:41:39 -0300

    feat: authorization interceptor configured

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/handlers/AuthorizationInterceptor.java
@@ -0,0 +1,16 @@
+package com.ciandt.summit.bootcamp2022.application.adapters.controllers.handlers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthorizationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+}
