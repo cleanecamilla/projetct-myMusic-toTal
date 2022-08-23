d61a5b6f4e54f915b0f52cc9144aa72a7506426b
rsousa <rsousa@ciandt.com>
2022-08-23 11:34:55 -0300

    feat: authorizeRequestTest refined to mock Token Provider client

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/unit/handlers/AuthorizationInterceptorTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/unit/handlers/AuthorizationInterceptorTest.java
@@ -1,12 +1,14 @@
 package com.ciandt.summit.bootcamp2022.unit.handlers;
 
-import com.ciandt.summit.bootcamp2022.SummitBootcampApplication;
 import com.ciandt.summit.bootcamp2022.application.adapters.controllers.MusicController;
+import com.ciandt.summit.bootcamp2022.domains.token.dto.CreateAuthorizer;
+import com.ciandt.summit.bootcamp2022.domains.token.dto.CreateAuthorizerData;
+import com.ciandt.summit.bootcamp2022.infra.feignclients.TokenProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -26,14 +28,37 @@ public class AuthorizationInterceptorTest {
     private MockMvc mockMvc;
 
     @MockBean
+    private TokenProvider tokenProvider;
+
+    @MockBean
     private MusicController musicController;
+
+    private final String TOKEN = "token";
+    private final String USER = "user";
+
+    private CreateAuthorizer fakeCreateAuthorizer;
+
+    @BeforeEach
+    public void setup() {
+        CreateAuthorizerData createAuthorizerData = new CreateAuthorizerData(TOKEN, USER);
+        fakeCreateAuthorizer = new CreateAuthorizer(createAuthorizerData);
+    }
 
     @Test
     public void authorizeRequestTest() throws Exception {
         String expected = "authorized";
-        when(musicController.get()).thenReturn(ResponseEntity.ok(expected));
 
-        mockMvc.perform(get("/api/v1/music"))
+        when(musicController.get())
+                .thenReturn(ResponseEntity.ok(expected));
+
+        when(tokenProvider.createTokenAuthorizer(fakeCreateAuthorizer))
+                .thenReturn(ResponseEntity.status(201).body("ok"));
+
+        mockMvc.perform(
+                    get("/api/v1/music")
+                        .header("token", TOKEN)
+                            .header("user", USER)
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().string(expected));
     }
