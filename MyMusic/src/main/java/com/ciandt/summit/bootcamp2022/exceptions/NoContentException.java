ee616b07d267f9ad3a7d55e9af97f03445982918
LaizaZnCIT <laiza@ciandt.com>
2022-08-23 16:20:42 -0300

    creating exceptions

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exceptions/NoContentException.java
@@ -0,0 +1,13 @@
+package com.ciandt.summit.bootcamp2022.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NO_CONTENT)
+public class NoContentException extends RuntimeException {
+    public NoContentException(String message) {
+        super(message);
+    }
+
+
+}
