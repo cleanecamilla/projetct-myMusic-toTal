67d90d5cd1690a2db4f3ab61d8eeea4d0b61ccf0
Rebecca Moraes <rgabrielle@ciandt.com>
2022-08-25 16:01:01 -0300

    BOOT-903 - Criar busca por artista ou música - Foi adicionado o validator no filtro

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -4,16 +4,21 @@ import com.ciandt.summit.bootcamp2022.service.MusicService;
 import com.ciandt.summit.bootcamp2022.service.dto.MusicDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/")
 @RequiredArgsConstructor
+@Validated
 public class MusicController {
 
     private final MusicService musicService;
@@ -25,7 +30,7 @@ public class MusicController {
 
     @GetMapping("/musicas")
     public ResponseEntity<Set<MusicDTO>> findAllWithFilterByName(
-            @RequestParam(name = "filtro", required = false) final String name){
+            @RequestParam(name = "filtro", required = false) @Size(min = 10, message = "The filter must have at least 2 characters")  final String name){
 
         return
                 ResponseEntity.ok(musicService.findAllWithFilterByName(name));
