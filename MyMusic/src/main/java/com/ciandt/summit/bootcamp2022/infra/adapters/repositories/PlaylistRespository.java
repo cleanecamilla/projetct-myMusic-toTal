95b513c70cac3df93fbfa9e5b806d8156a4994ba
Jenifer <jnakabar@ciandt.com>
2022-08-25 10:45:42 -0300

    refect:test method changed

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/PlaylistRespository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/PlaylistRespository.java
@@ -1,9 +1,7 @@
 package com.ciandt.summit.bootcamp2022.infra.adapters.repositories;
 
 import com.ciandt.summit.bootcamp2022.domains.playlists.ports.repositories.PlaylistRespositoryPort;
-import com.ciandt.summit.bootcamp2022.domains.songs.Song;
 import com.ciandt.summit.bootcamp2022.infra.adapters.entities.PlaylistEntity;
-import com.ciandt.summit.bootcamp2022.infra.adapters.entities.SongEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,16 +20,8 @@ public class PlaylistRespository implements PlaylistRespositoryPort {
     }
 
     @Override
-    public void addSong(String id, Song song) throws RuntimeException {
-        Optional<PlaylistEntity> playlist = this.findById(id);
-        if (playlist.isPresent()) {
-            SongEntity songEntity = new SongEntity(song);
-            playlist.get().getSongs().add(songEntity);
-            playlistRepository.save(playlist.get());
-        } else {
-            throw new RuntimeException();
-        }
-
+    public void addSong(PlaylistEntity playlistEntity) {
+        playlistRepository.save(playlistEntity);
     }
 
 }
