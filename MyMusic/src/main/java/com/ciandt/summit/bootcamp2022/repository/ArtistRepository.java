f46e9309bb311db91dee4ce75127844f880ea222
Jefferson R Silva <108823027+jeffrhudson@users.noreply.github.com>
2022-08-30 12:10:59 -0300

    Revert "Boot 915"

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/ArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ciandt.summit.bootcamp2022.repository;
-
-import com.ciandt.summit.bootcamp2022.entity.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistRepository extends JpaRepository<Artist, String> { }
-
